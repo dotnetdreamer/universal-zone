63139ac800c3ab31bc4203affd7fc235a76966e6
Idrees Khan <idrees.khan@vanderlande.com>
2024-07-31 14:21:03 +0200

    added directives module

--- uz-workspace/projects/ngx-universal-zone/ui/index.ts
+++ uz-workspace/projects/ngx-universal-zone/ui/index.ts
@@ -1,2 +1,4 @@
 export * from './no-data/no-data.component';
 export * from './no-data/no-data.module';
+export * from './directives/directives.module';
+export * from './directives/only-number.directive';
