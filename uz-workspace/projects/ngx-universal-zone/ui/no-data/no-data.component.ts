f2f3913f980e367d0ae3df53115aa85f2b77510a
Idrees Khan <idrees.khan@vanderlande.com>
2024-08-01 21:24:24 +0200

    fixed no-data

--- uz-workspace/projects/ngx-universal-zone/ui/no-data/no-data.component.ts
+++ uz-workspace/projects/ngx-universal-zone/ui/no-data/no-data.component.ts
@@ -5,12 +5,8 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   selector: 'app-no-data',
   template: `
     <div class="data-empty">
-       @if(iconHtml) {
-        <div [innerHtml]="iconHtml"></div>
-       }
-       @if(message()) {
-        <p>{{ message() }}</p>
-       }
+        <div *ngIf="iconHtml" [outerHTML]="iconHtml"></div>
+        <p *ngIf="message()">{{ message() }}</p>
       <ng-content></ng-content>
     </div>
   `,
@@ -21,9 +17,9 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
         padding-top: 20%;
       }
 
-      .data-empty ion-icon {
+      .data-empty svg {
         color: #efefef;
-        font-size: 100px;
+        width: 120px;
       }
     `,
   ],
@@ -37,6 +33,36 @@ export class NoDataComponent implements OnInit {
   constructor(private domSanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    this.iconHtml = this.domSanitizer.bypassSecurityTrustHtml(this.icon());
+    let svg = this.icon();
+    if(!this.icon()) {
+      svg = `<?xml version="1.0" encoding="utf-8"?>
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 40 40" style="enable-background:new 0 0 40 40;" xml:space="preserve">
+      <style type="text/css">
+        .st0{fill-rule:evenodd;clip-rule:evenodd;}
+      </style>
+      <g>
+        <path d="M9,21.8c0,0.4,0.3,0.7,0.6,0.8c0.5,0.1,0.9-0.3,0.9-0.8v-7h6.2V8.4h11.9c1.3,0,2.4,1.1,2.4,2.4v5.8c0,0.5,0.4,0.8,0.9,0.8
+          c0.4-0.1,0.6-0.4,0.6-0.8v-5.7c0-2.2-1.8-3.9-3.9-3.9H13.3L9,11.7V21.8z M14,8.4h1.2v4.9h-4.7v-1L14,8.4z"/>
+        <path d="M31.7,29.9c-0.4,0.1-0.6,0.4-0.6,0.8v2.2c0,1.3-1.1,2.4-2.4,2.4H12.9c-1.3,0-2.4-1.1-2.4-2.4v-1.5c0-0.5-0.4-0.8-0.9-0.8
+          C9.2,30.7,9,31,9,31.4v1.4c0,2.2,1.8,3.9,3.9,3.9h15.7c2.2,0,3.9-1.8,3.9-3.9v-2.2C32.5,30.2,32.1,29.8,31.7,29.9z"/>
+        <path d="M8.2,7.5c0.2,0.2,0.3,0.2,0.5,0.2c0.2,0,0.4-0.1,0.6-0.3c0.3-0.3,0.2-0.8-0.1-1.1L5.8,3c-0.3-0.3-0.8-0.3-1.1,0
+          C4.5,3.2,4.4,3.7,4.7,4L8.2,7.5z"/>
+        <path d="M10.5,4.7C10.6,5,11,5.2,11.3,5.2c0.2,0,0.4-0.1,0.5-0.2C12,4.8,12.1,4.5,12,4.2l-0.8-2.4c-0.1-0.4-0.6-0.7-1.1-0.4
+          c-0.3,0.2-0.5,0.6-0.4,1L10.5,4.7z"/>
+        <path d="M6.4,8.8L4,8C3.6,7.9,3.1,8.1,3,8.5C2.9,8.9,3.2,9.3,3.6,9.5l2.4,0.8c0.1,0,0.2,0,0.3,0c0.3,0,0.7-0.2,0.8-0.6
+          C7,9.3,6.8,9,6.4,8.8z"/>
+        <path d="M35.7,19.8c-0.5-0.3-1.1-0.2-1.5,0.2L32,22.3l-2.2-2.2c-0.5-0.5-1.2-0.4-1.7,0c-0.4,0.5-0.4,1.2,0.1,1.6l2.2,2.2l-2.2,2.2
+          c-0.4,0.4-0.5,1.1-0.1,1.6c0.2,0.3,0.6,0.4,0.9,0.4c0.3,0,0.6-0.1,0.8-0.3l2.2-2.2l2.3,2.3c0.2,0.2,0.5,0.3,0.8,0.3
+          c0.3,0,0.6-0.1,0.9-0.4c0.4-0.5,0.3-1.2-0.1-1.6l-2.3-2.3l2.3-2.3C36.4,21.1,36.3,20.3,35.7,19.8z"/>
+        <path d="M10.2,29.4c1.1-0.2,1.9-1.1,2.1-2.1c0.4-1.9-1.3-3.5-3.2-3.2c-1.1,0.2-1.9,1.1-2.1,2.1C6.6,28.2,8.3,29.8,10.2,29.4z
+          M8.5,27c-0.2-0.8,0.5-1.5,1.4-1.4c0.4,0.1,0.8,0.5,0.9,0.9c0.2,0.8-0.5,1.5-1.4,1.4C9,27.8,8.6,27.5,8.5,27z"/>
+        <path d="M36.8,7.2c-0.7-1.1-2.2-1.1-2.9,0c-0.3,0.5-0.3,1.1,0,1.6c0.3,0.6,0.9,0.8,1.5,0.8s1.1-0.3,1.5-0.8
+          C37.1,8.3,37.1,7.7,36.8,7.2z M35.7,8.4c-0.2,0.2-0.5,0.2-0.7,0c-0.2-0.2-0.2-0.5,0-0.7c0.1-0.1,0.2-0.1,0.4-0.1
+          c0.1,0,0.3,0,0.4,0.1C35.9,7.8,35.9,8.2,35.7,8.4z"/>
+      </g>
+      </svg>`;
+    }
+    this.iconHtml = this.domSanitizer.bypassSecurityTrustHtml(svg);
   }
 }
