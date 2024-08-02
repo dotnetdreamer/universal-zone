576b996d9757c5d766a531aa454c79abb81107c5
devfaisalkhan <dev.faisalkhan@gmail.com>
2024-08-02 15:26:30 +0500

    added new only nuimber directive

--- uz-workspace/projects/ngx-universal-zone/ui/directives/only-number.directive.ts
+++ uz-workspace/projects/ngx-universal-zone/ui/directives/only-number.directive.ts
@@ -1,17 +1,14 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[onlyNumber]'
 })
 export class OnlyNumberDirective {
+  constructor() { }
 
-  constructor(private _el: ElementRef) { }
-
-  @HostListener('input', ['$event']) onInputChange(event) {
-    const initialValue = this._el.nativeElement.value;
-    this._el.nativeElement.value = initialValue.replace(/[^0-9]*/g, '');
-    if ( initialValue !== this._el.nativeElement.value) {
-      event.stopPropagation();
+  @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
+    if (event.key === '.') {
+      event.preventDefault();
     }
   }
 }
